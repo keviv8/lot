7dccf18316f4199f0c1c7f9d0b2a5b025bd184db
vkolla <vkolla@pps.co.za>
2020-06-13 14:32:42 +0200

    added properties file
    Signed-off-by: vkolla <vkolla@pps.co.za>

--- src/main/java/mainer/App.java
+++ src/main/java/mainer/App.java
@@ -4,32 +4,38 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import io.restassured.http.Cookie;
 import io.restassured.response.Response;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.text.ParseException;
+import java.util.Properties;
 
 import static io.restassured.RestAssured.given;
 /**
  * Hello world!
  */
 public class App {
+    private static Properties prop = new Properties();
+
     public static void main(String[] args) throws IOException, ParseException {
-        System.out.println("Hello World!");
-        System.out.println(productDetails().getStatusCode());
-        System.out.println(productDetails().getBody().asString());
+        readProps();
+        Response pd = productDetails();
         Response ci = cartItem();
+        System.out.println(pd.getStatusCode());
+        System.out.println(pd.getBody().asString());
         System.out.println(ci.getStatusCode());
         System.out.println(ci.getBody().asString());
     }
 
     public static Response productDetails() {
         Response prodDetails_response;
-        prodDetails_response = given().when().get("https://api.takealot.com/rest/v-1-9-1/product-details/PLID68128803?platform=desktop&size=Single");
+        prodDetails_response = given().when().get(prop.getProperty("product.details").replace("$product_id", "PLID68128803"));
         return prodDetails_response;
     }
 
     public static Response guestLogin() {
         Response guestLogin_response;
-        guestLogin_response = given().when().post("https://api.takealot.com/rest/v-1-9-1/guests/login");
+        guestLogin_response = given().when().post(prop.getProperty("guest.login"));
         return guestLogin_response;
     }
 
@@ -45,6 +51,7 @@ public class App {
         String guest_id = readKeyValueStringFromJSON("guest_id", gLogin.getBody().asString());
         System.out.println("csrf_id " + csrf_id);
         String payload = "{\"products\":[{\"id\":92247807,\"quantity\":1,\"enhancedEcommerceAddToCart\":{\"ecommerce\":{\"add\":{\"products\":[{\"category\":\"Home & Kitchen/Homeware/Furniture/Bedroom Furniture/Beds\",\"name\":\"Postucare Limitless Mattress Only - 2 Comfort Levels\",\"dimension1\":\"Single\",\"price\":3399,\"variant\":null,\"id\":\"PLID68128803\",\"position\":0,\"brand\":\"Postucare\",\"quantity\":1}]},\"currencyCode\":\"ZAR\"},\"event\":\"addToCart\"}}]}";
+        String payloader = prop.getProperty("cartitem.payload");
         cartAdd_response = given().
                 when().
                 cookie(gLogin_cookie_taid).
@@ -52,17 +59,24 @@ public class App {
                 cookie(gLogin_cookie_tausr).
                 cookie(gLogin_cookie_cfduid).
                 header("x-csrf-token", csrf_id).
-                body(payload).
-                post("https://api.takealot.com/rest/v-1-9-1/customers/" + guest_id + "/cart/items");
+                body(payloader).
+                post(prop.getProperty("add.cart").replace("$guest_id", guest_id));
         return cartAdd_response;
+    }
+
+    public static void readProps() {
+        try (InputStream input = new FileInputStream("dot.properties")) {
+            prop = new Properties();
+            prop.load(input);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static String readKeyValueStringFromJSON(String variablename, String json) throws IOException, ParseException {
         ObjectMapper om = new ObjectMapper();
-//        JsonNode node = om.readTree(readJsonFromFile("C:\\Users\\keviv\\Documents\\Project Arena\\filezilla_rob\\RetrieveParty\\t_1571897769836\\response.json"));
         JsonNode node = om.readTree(json);
         JsonNode string = node.findValue(variablename);
-//        System.out.println("Path " + node.findPath("responseObjects").get(0));
         return string.toString();
     }
 }
